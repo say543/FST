5fc6cdfca1d23bca2afbd64952a8a44dae030d4b
Chieh-Chun Chang <say543@msn.com>
2019-07-30 16:48:31 -0700

    Create  files_dataset_extra_intent.py

---
+++ files domain/files_dataset_extra_intent.py
@@ -0,0 +1,35 @@
+import codecs;
+import random;
+
+# add hyper paramter if unbalanced
+hyper_parameter = 200
+
+# add repeated times
+repated_time = 10
+
+
+fileDomainRelatedIntent = ['file_search', 'file_open', 'file_share', 'file_download', 'file_other']
+
+OutputSet = [];
+
+with codecs.open('files_dataset.tsv', 'r', 'utf-8') as fin:
+    for line in fin:
+        line = line.strip();
+        if not line:
+            continue;
+        linestrs = line.split("\t")
+        if len(linestrs) < 5:
+            continue;
+        for i in range(0,repated_time):
+            OutputSet.append(linestrs[0]+"\t\t"+linestrs[1]+"\t"+linestrs[2]);
+
+"""
+# comment shuffle in the first place
+#random.shuffle(OutputSet);
+"""
+
+with codecs.open('files_dataset_intent.tsv', 'w', 'utf-8') as fout:
+    for item in OutputSet:
+        fout.write(item + '\r\n');
+
+        
