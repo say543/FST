e48ff81e563e382bac0424b18832e662b3f9e642
Chieh-Chun Chang <say543@msn.com>
2019-09-09 20:40:54 -0700

    Update slot_collect_all_features.py

--- files domain/slot_collect_all_features.py
+++ files domain/slot_collect_all_features.py
@@ -2,15 +2,30 @@ import glob;
 import codecs;
 import random;
 import os
+from shutil import copyfile
+
+
+
 
 
 outputFile = 'files_slot_training.tsv'
+# replace directly
+outputTrainingFolderFile = '..\\files_slot_training.tsv'
+outputFileWithSource = "files_slot_training_with_source.tsv"
 files = glob.glob("*.tsv");
 outputs = [];
+outputsWithSource = [];
+
+
+# copy file from data folder directly
+# update more in the future
+copyfile("..\\files_mystuff_after_filtering.tsv" , "files_mystuff_after_filtering.tsv")
+copyfile("..\\teams_slot_training_after_filtering.tsv" , "teams_slot_training_after_filtering.tsv")
+
 
 for file in files:
 
-    if file == outputFile:
+    if file == outputFile or file == outputFileWithSource:
         continue;
     
     print("collecting: " + file);
@@ -23,16 +38,28 @@ for file in files:
             if len(array) < 5:
                 print("error:" + line);
             outputs.append(line);
+            outputsWithSource.append(line+'\t'+ file);
 
 print('shuffling');
 random.seed(0.1);
 random.shuffle(outputs);
 
 outputs = ['\t'.join(['id', 'query', 'intent', 'domain', 'QueryXml', 'id', '0'])] + outputs;
+outputsWithSource = ['\t'.join(['id', 'query', 'intent', 'domain', 'QueryXml', 'id', '0', 'source'])] + outputsWithSource;
 
 
 
 
 with codecs.open(outputFile, 'w', 'utf-8') as fout:
+    for item in outputs:
+        fout.write(item + '\r\n');
+
+with codecs.open(outputFileWithSource, 'w', 'utf-8') as fout:
+    for item in outputsWithSource:
+        fout.write(item + '\r\n');
+
+# replace directly
+# if do not want , comment this
+with codecs.open(outputTrainingFolderFile, 'w', 'utf-8') as fout:
     for item in outputs:
         fout.write(item + '\r\n');
