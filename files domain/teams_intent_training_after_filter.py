0abe9c5ee60db4091bfbc3ea064ff527f51e6c7b
Chieh-Chun Chang <say543@msn.com>
2019-09-16 17:47:59 -0700

    Update teams_intent_training_after_filter.py

--- files domain/teams_intent_training_after_filter.py
+++ files domain/teams_intent_training_after_filter.py
@@ -5,12 +5,21 @@ import random;
 hyper_parameter = 200
 
 # 07132019 add file_navigate
-fileDomainRelatedIntent = ['file_search', 'file_open', 'file_share', 'file_download', 'file_other']
+#fileDomainRelatedIntent = ['file_search', 'file_open', 'file_share', 'file_download', 'file_other']
 # no need to add file nvatigate for intent training
 # only slot is needed
+# add teamspace_search but filter with different rules 
 #fileDomainRelatedIntent = ['file_search', 'file_open', 'file_share', 'file_download', 'file_other', 'file_navigate']
+fileDomainRelatedIntent = ['file_search', 'file_open', 'file_share', 'file_download', 'file_other', 'file_navigate', 'teamspace_search']
 
-OutputSet = [];
+
+##############################
+# intent level candidate
+##############################
+teamspaceSearchCandidateSet = set()
+
+
+Output = [];
 
 with codecs.open('teams_intent_training.tsv', 'r', 'utf-8') as fin:
     for line in fin:
@@ -24,16 +33,67 @@ with codecs.open('teams_intent_training.tsv', 'r', 'utf-8') as fin:
         if linestrs[3] in fileDomainRelatedIntent:
 
             # if file_navigate
-            
-            OutputSet.append(line);
+
+            # document teamspace_search
+            # for further analysis
+            # skip it at first
+            if linestrs[3] == "teamspace_search":
+                # having problem or tab
+                # then skip
+                if linestrs[2].find("program") != -1 or \
+                   linestrs[2].find("tab") != -1 or \
+                   linestrs[2].find("channel") != -1 or \
+                   linestrs[2].find("team") != -1 or \
+                   linestrs[2].find("teams") != -1 or \
+                   linestrs[2].find("conversation") != -1 or \
+                   linestrs[2].find("chat") != -1:
+                    teamspaceSearchCandidateSet.add(line)
+                    continue
+                linestrs[3] = "file_search"
+
+            # following the guideline to move queries to file_search intent
+            verbs = set(["search ",
+                        "Search ",
+                        "find ",
+                        "show ",
+                        "Show ",
+                        "Open ",
+                        "open ",
+                        "Find ",
+                        "find ",
+                        "display ",
+                        "Display ",
+                        "locate ",
+                        "Locate ",
+                        ])
+
+            if linestrs[3] == "file_navigate":
+
+                for verb in verbs:
+                    if linestrs[2].find(verb) != -1:
+                        linestrs[3] = "file_search"
+                        break
+
+            # TurnNumber	PreviousTurnIntent	query	intent
+            Output.append(linestrs[0]+"\t"+linestrs[1]+"\t"+linestrs[2]+"\t"+linestrs[3]);
 
 """
 # comment shuffle in the first place
 #random.shuffle(OutputSet);
 """
 
+
+#######################
+# intent level output
+#######################
 with codecs.open('teams_intent_training_after_filtering.tsv', 'w', 'utf-8') as fout:
-    for item in OutputSet:
+    for item in Output:
+        fout.write(item + '\r\n');
+
+
+        
+with codecs.open('teams_intent_training_after_filtering_teamspace_search.tsv', 'w', 'utf-8') as fout:
+    for item in teamspaceSearchCandidateSet:
         fout.write(item + '\r\n');
 
         
