3d77b05cf8363dd9ee6fd44a463f66dd9399a9b1
Chieh-Chun Chang <say543@msn.com>
2020-01-29 17:21:56 -0800

    Update inmeeting_common_intent_extractor.py

--- files domain/inmeeting_common_intent_extractor.py
+++ files domain/inmeeting_common_intent_extractor.py
@@ -32,7 +32,7 @@ with codecs.open('InMeeting_Intent_Training_01292020v1.tsv', 'r', 'utf-8') as fi
             continue;
 
         # make sure it is in common intent
-        if linestrs[2] in commonDomainRelatedIntent:        
+        if linestrs[3] in commonDomainRelatedIntent:        
             OutputSet.append(linestrs[0]+"\t\t"+linestrs[2]+"\t"+linestrs[3]);        
 
 # commna shuffle at first
