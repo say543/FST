9fae38c6f400db509a4804c79a22ecfe037f32f1
Chieh-Chun Chang <say543@msn.com>
2020-02-28 12:05:13 -0800

    Update files_other_training_rewrite_intent.py

--- files domain/files_other_training_rewrite_intent.py
+++ files domain/files_other_training_rewrite_intent.py
@@ -4,6 +4,10 @@ import random;
 # add hyper paramter if unbalanced
 hyper_parameter = 200
 
+
+
+commonDomainRelatedIntent = ['cancel','confirm','reject','select_none']
+
 rewriteIntent = "file_other" 
 
 OutputSet = [];
@@ -14,8 +18,11 @@ with codecs.open('files_other_training.tsv', 'r', 'utf-8') as fin:
             continue;
         linestrs = line.split("\t")
         if len(linestrs) < 3:
-            continue;        
-        OutputSet.append(linestrs[0]+"\t\t"+linestrs[2]+"\t"+rewriteIntent);        
+            continue;
+
+        #skip common domain intent
+        if linestrs[3] not in commonDomainRelatedIntent:
+            OutputSet.append(linestrs[0]+"\t\t"+linestrs[2]+"\t"+rewriteIntent);        
 
 # commna shuffle at first
 #random.shuffle(OutputSet);
