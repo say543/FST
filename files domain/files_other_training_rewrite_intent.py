ea250dc1e4acee6dc67860f17cebde3a25346879
Chieh-Chun Chang <say543@msn.com>
2020-02-28 15:12:21 -0800

    Update files_other_training_rewrite_intent.py

--- files domain/files_other_training_rewrite_intent.py
+++ files domain/files_other_training_rewrite_intent.py
@@ -21,13 +21,24 @@ with codecs.open('files_other_training.tsv', 'r', 'utf-8') as fin:
             continue;
 
         #skip common domain intent
+        #if linestrs[3] not in commonDomainRelatedIntent:
+        #    OutputSet.append(linestrs[0]+"\t\t"+linestrs[2]+"\t"+rewriteIntent);
+
+        # add
+        # "PreviousTurnDomain"
+        # "PreviousTurnIntent"
+        # as
+        # 'TurnNumber', PREVIOUSTURNINTENT, 'query', 'intent',PREVIOUSTURNDOMAIN])
+        # append empty at first
         if linestrs[3] not in commonDomainRelatedIntent:
-            OutputSet.append(linestrs[0]+"\t\t"+linestrs[2]+"\t"+rewriteIntent);        
+            OutputSet.append(linestrs[0]+"\t\t"+linestrs[2]+"\t"+rewriteIntent+"\t");
+
 
 # commna shuffle at first
 #random.shuffle(OutputSet);
 
 with codecs.open('files_other_training_after_rewrite.tsv', 'w', 'utf-8') as fout:
+    fout.write('\t'.join(['TurnNumber', 'PreviousTurnIntent', 'query', 'intent','PreviousTurnDomain'])+'\r\n');
     for item in OutputSet:
         fout.write(item + '\r\n');
     
