10586f9b0f7d855223c08d9e3f16a1927282c6b3
Chieh-Chun Chang <say543@msn.com>
2020-02-28 11:46:53 -0800

    Update calendar_common_intent_extractor.py

--- files domain/calendar_common_intent_extractor.py
+++ files domain/calendar_common_intent_extractor.py
@@ -31,9 +31,26 @@ with codecs.open('calendar.train.intentsslots.generated.tsv', 'r', 'utf-8') as f
         if len(linestrs) < 4:
             continue;
 
+
+        # for debug 
+        #print(len(linestrs))
+        #print(line)
+
         # make sure it is in common intent
+        #if linestrs[3] in commonDomainRelatedIntent:        
+        #    OutputSet.append(linestrs[0]+"\t\t"+linestrs[2]+"\t"+linestrs[3]);
+
+        # add
+        # "PreviousTurnDomain"
+        # "PreviousTurnIntent"
+        # as
+        # 'TurnNumber', PREVIOUSTURNINTENT, 'query', 'intent',PREVIOUSTURNDOMAIN])
+        # since calendar some queries might miss two columns
+        # append empty at first
         if linestrs[3] in commonDomainRelatedIntent:        
-            OutputSet.append(linestrs[0]+"\t\t"+linestrs[2]+"\t"+linestrs[3]);        
+            #OutputSet.append(linestrs[0]+"\t"+linestrs[7]+"\t"+linestrs[2]+"\t"+linestrs[3]+"\t"+linestrs[6]);
+            OutputSet.append(linestrs[0]+"\t\t"+linestrs[2]+"\t"+linestrs[3]+"\t");
+        
 
 # commna shuffle at first
 #random.shuffle(OutputSet);
